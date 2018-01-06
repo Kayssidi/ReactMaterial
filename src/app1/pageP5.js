50f0a983a76a7d3467c458ec1f4f5b9506814733
KCD <Kayssidi@users.noreply.github.com>
2018-01-06 22:32:56 +0100

    integrate proper standalone P5Wrapper and move sketch to its own file

--- src/app1/pageP5.js
+++ src/app1/pageP5.js
@@ -2,12 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "material-ui/styles";
 
-import P5Wrapper from './p5/p5Wrapper';
 import MyAppBar from './myAppBar';
+import P5Wrapper from './p5/p5Wrapper';
+import sketcher from './p5/WalkerP5';
 
 const styles = {};
 
 class PageP5 extends React.Component {
+  
   state = {};
 
   render() {
@@ -15,7 +17,7 @@ class PageP5 extends React.Component {
     return (
       <div>
         <MyAppBar title="P5 Tester" />
-        <P5Wrapper />
+        <P5Wrapper sketch={sketcher}/>
       </div>
     );
   }
